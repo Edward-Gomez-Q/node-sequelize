d17ced916d2c0beea4e0acc0a6c55f06e7bdf43c
Edward-Gomez-Q <osval_edward_13@hotmail.com>
2025-05-06 13:51:58 -0400

    Obtener y Crear para Clientes y Pedidos

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const clienteRoutes = require('./routes/cliente.routes');
+const pedidoRoutes = require('./routes/pedido.routes');
 
-var app = express();
-
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', clienteRoutes);
+app.use('/api', pedidoRoutes);
 
 module.exports = app;
